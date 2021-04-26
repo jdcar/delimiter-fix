30975ccdd3a680fc6996aa9f59f90641bdd2a3a0
jdcar <jdcarlstone@gmail.com>
2021-04-26 08:57:07 -0500

    Add pipe

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ const delimiterChange = () => {
     } 
 
     const oldMarc = marcInput.val()
-    const newMarcEdit = oldMarc.replaceAll(/\$|ǂ|‡/g, "$").trim()
-    const newOclc = oldMarc.replaceAll(/\$|ǂ|‡/g, "ǂ").trim()
-    const newAlma = oldMarc.replaceAll(/\$|ǂ|‡/g, "‡").trim()
-    const newNoDelimiter = oldMarc.replaceAll(/\$.|ǂ.|‡./g, "").replaceAll(/  /g, " ").trim()
-    const newEmdash = oldMarc.replaceAll(/\$. |ǂ. |‡. /g, "—").replaceAll(/ —/g, "—").replace(/^—/, "").trim()
-
+    const newMarcEdit = oldMarc.replaceAll(/\$|ǂ|‡|\|/g, "$").trim()
+    const newOclc = oldMarc.replaceAll(/\$|ǂ|‡|\|/g, "ǂ").trim()
+    const newAlma = oldMarc.replaceAll(/\$|ǂ|‡|\|/g, "‡").trim()
+    const newNoDelimiter = oldMarc.replaceAll(/\$.|ǂ.|‡.|\|./g, "").replaceAll(/  /g, " ").trim()
+    const newEmdash = oldMarc.replaceAll(/\$. |ǂ. |‡. |\|. /g, "—").replaceAll(/ —/g, "—").replace(/^—/, "").trim()
+    const newPipe = oldMarc.replaceAll(/\$|ǂ|‡|\|/g, "|").trim()
+    
 
     console.log(newMarcEdit)
     console.log(newOclc)
@@ -41,7 +42,7 @@ const delimiterChange = () => {
     $('#new-oclc').append(newOclc)
     $('#new-no').append(newNoDelimiter)
     $('#new-emdash').append(newEmdash)
-
+    $('#new-pipe').append(newPipe)
 
 }
 
